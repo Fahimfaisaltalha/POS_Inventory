52d30300eec512936feb1c8e9399eb00965b53e4
Fahim Faisal Talha <fahimtalha9@gmail.com>
2025-08-15 00:27:43 +0600

    Add product table

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+
+    }
+    public function store(Request $request)
+    {
+ 
+    }
+    public function edit(Product $product)
+    {
+
+    }
+    public function update(Request $request)
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\LogoutController;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\User\ProfileController;
 use App\Http\Middleware\JwtTokenVerify;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register',[RegisterController::class,'register']);
@@ -16,3 +18,9 @@ Route::post('login',[LoginController::class,'login']);
 
 Route::get('profile',[ProfileController::class,'profile'])->middleware(JwtTokenVerify::class);
 Route::post('logout',[LogoutController::class,'logout'])->middleware(JwtTokenVerify::class);
+
+Route::group(['prefix' => 'products'],function(){
+    Route::get('/',[ProductController::class,'index']);
+
+
+})->middleware(JwtTokenVerify::class);
